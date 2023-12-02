42d99ec3eff46734e732e2505f058c2d50345af3
Zurihaqi <zurihaqi@gmail.com>
2023-12-02 23:37:51 +0700

    fix image not showing after search

--- resources/views/pemancing/search.blade.php
+++ resources/views/pemancing/search.blade.php
@@ -1,117 +1,117 @@
 @extends('layouts.pemancing')
 @section('css')
-    <style>
-        .newBackground {
-            background-image: url('images/cover_cargo.png');
-            background-size: cover;
-            background-position: center;
-            background-repeat: no-repeat;
-            height: 580px;
+<style>
+    .newBackground {
+        background-image: url('images/cover_cargo.png');
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+        height: 580px;
+        width: 100%;
+    }
+
+    .newBackground::before {
+        content: "";
+        display: block;
+        position: absolute;
+        top: 0;
+        left: 0;
+        height: 655px;
+        width: 100%;
+        background-color: rgba(0, 0, 0, 0.5);
+    }
+
+    .content {
+        position: absolute;
+        top: 35%;
+        left: 20%;
+        transform: translate(-50%, -50%);
+        color: white;
+        font-size: 4vw;
+        font-weight: bold;
+        text-align: center;
+    }
+
+    @media (max-width: 768px) {
+        .container {
+            flex-direction: column;
+        }
+
+        .newBackground,
+        .content {
             width: 100%;
+            left: 50%;
         }
 
         .newBackground::before {
-            content: "";
-            display: block;
-            position: absolute;
-            top: 0;
-            left: 0;
             height: 655px;
-            width: 100%;
-            background-color: rgba(0, 0, 0, 0.5);
+        }
+    }
+
+    @media (max-width: 480px) {
+        h1 {
+            font-size: 24px;
         }
 
-        .content {
-            position: absolute;
-            top: 35%;
-            left: 20%;
-            transform: translate(-50%, -50%);
-            color: white;
-            font-size: 4vw;
-            font-weight: bold;
-            text-align: center;
+        p {
+            font-size: 16px;
         }
+    }
 
-        @media (max-width: 768px) {
-            .container {
-                flex-direction: column;
-            }
+    .content1 {
+        width: 80%;
+        margin-top: 60px;
+        margin-left: 10%;
+        margin-bottom: 50px;
+    }
 
-            .newBackground,
-            .content {
-                width: 100%;
-                left: 50%;
-            }
-
-            .newBackground::before {
-                height: 655px;
-            }
-        }
-
-        @media (max-width: 480px) {
-            h1 {
-                font-size: 24px;
-            }
-
-            p {
-                font-size: 16px;
-            }
-        }
-
-        .content1 {
-            width: 80%;
-            margin-top: 60px;
-            margin-left: 10%;
-            margin-bottom: 50px;
-        }
-
-        .search {
-            width: 80%;
-            margin-top: 30px;
-            margin-left: 10%;
-        }
-    </style>
+    .search {
+        width: 80%;
+        margin-top: 30px;
+        margin-left: 10%;
+    }
+</style>
 @endsection
 @section('content')
-    <div class="search">
-        <form action="{{ route('pemancing.search') }}" method="POST">
-            @csrf
-            <div class="row">
-                <div class="col-sm-4">
-                    <input type="text" name="search" class="form-control" placeholder="Cari..">
-                </div>
-                <div class="col-sm-3">
-                    <button type="submit" class="btn btn-primary"><i class="fa fa-search"></i>Cari</button>
-                </div>
+<div class="search">
+    <form action="{{ route('pemancing.search') }}" method="POST">
+        @csrf
+        <div class="row">
+            <div class="col-sm-4">
+                <input type="text" name="search" class="form-control" placeholder="Cari..">
             </div>
-        </form>
-    </div>
-    @if ($status == 'false')
-        <p style="margin-top: 20px; margin-left: 10%">Data Tidak Ditemukan <br>
-            Status : {{ $status }}
-        </p>
-    @else
-        <p style="margin-top: 20px; margin-left: 10%">Data Ditemukan <br>
-            pada Index ke : {{ $hasilIndex }}
-            <br>
-            Status : {{ $status }}
-        </p>
-        <div class="content1">
-            <div class="row">
-                <div class="col-sm-4" style="margin-top: 10px">
-                    <div class="card" style="width: 320px; height: 360px">
-                        <img src="{{ asset('foto/' . $hasilData->gambar) }}" class="card-img-top" style="height: 200px">
-                        <div class="card-body">
-                            <h5 class="card-title">{{ $hasilData->nama }}</h5>
-                            <p class="card-text" style="font-size: 12px">{{ substr($hasilData->deskripsi, 0, 50) . '...' }}
-                            </p>
-                            <a href="{{ route('pemancing.detail', $hasilData->id) }}" class="btn btn-primary">Detail</a>
-                        </div>
-                    </div>
+            <div class="col-sm-3">
+                <button type="submit" class="btn btn-primary"><i class="fa fa-search"></i>Cari</button>
+            </div>
+        </div>
+    </form>
+</div>
+@if ($status == 'false')
+<p style="margin-top: 20px; margin-left: 10%">Data Tidak Ditemukan <br>
+    Status : {{ $status }}
+</p>
+@else
+<p style="margin-top: 20px; margin-left: 10%">Data Ditemukan <br>
+    pada Index ke : {{ $hasilIndex }}
+    <br>
+    Status : {{ $status }}
+</p>
+<div class="content1">
+    <div class="row">
+        <div class="col-sm-4" style="margin-top: 10px">
+            <div class="card" style="width: 320px; height: 360px">
+                <img src="{{ $hasilData->gambar }}" class="card-img-top" style="height: 200px">
+                <div class="card-body">
+                    <h5 class="card-title">{{ $hasilData->nama }}</h5>
+                    <p class="card-text" style="font-size: 12px">{{ substr($hasilData->deskripsi, 0, 50) . '...' }}
+                    </p>
+                    <a href="{{ route('pemancing.detail', $hasilData->id) }}" class="btn btn-primary">Detail</a>
                 </div>
             </div>
         </div>
-    @endif
+    </div>
+</div>
+@endif
 @endsection
 
 @section('javascript')
